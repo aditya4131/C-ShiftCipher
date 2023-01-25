bda7b2266e538a36c13a80cb416c73d9594efcc5
aditya4131 <asriva8@lsu.edu>
2023-01-25 12:05:39 -0600

    First Commit.

---
+++ main.c
@@ -0,0 +1,17 @@
+//A project to implement ceasar shift cipher.
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    char stringInput[20] = "";
+
+    printf("Enter Name:\n");
+
+    scanf("%19s", stringInput);
+
+    printf("Name: %s", stringInput);
+
+    return 0;
+}
